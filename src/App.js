6bbfbe5cf38d40d2b2df21cc42a652767ee12aec
Nikola Kranjcec <kranjcecni@gmail.com>
2023-09-05 21:42:06 +0200

    naziv sobe

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AppHeader from "./components/AppHeader";
 import Members from "./components/Members";
 import LogIn from "./components/LogIn";
 function App() {
-  const roomName = "observable-algebra3-room";
+  const roomName = "observable-algebra4-room";
   const scaledronKey = "ORrZ5bWBx7n0pp4E";
   function randomColor() {
     return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
